672492bc50db5d1e5b9efe986cbf1fd68c554d35
Luca Romagnoli <lromagnoli@zaizi.com>
2023-01-18 11:41:35 +0000

    use double quotes

--- encode_realm.py
+++ encode_realm.py
@@ -3,7 +3,7 @@ import base64
 
 
 def encode():
-    with open('ayr-realm.json') as realm_file:
+    with open("ayr-realm.json") as realm_file:
         realm = json.load(realm_file)
     realm_string = json.dumps(realm).encode()
     return base64.b64encode(realm_string)
@@ -11,7 +11,7 @@ def encode():
 
 def main():
     encoded_realm = encode()
-    with open('ayr-realm-b64', 'wb') as realm_string_file:
+    with open("ayr-realm-b64", "wb") as realm_string_file:
         realm_string_file.write(encoded_realm)
 
 
