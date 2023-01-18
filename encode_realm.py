e695176dc0de94d562410e4a89bb93976fc108c3
Luca Romagnoli <lromagnoli@zaizi.com>
2023-01-18 10:55:17 +0000

    add encode_realm.py

---
+++ encode_realm.py
@@ -0,0 +1,19 @@
+import json
+import base64
+
+
+def encode():
+    with open('ayr-realm.json') as realm_file:
+        realm = json.load(realm_file)
+    realm_string = json.dumps(realm).encode()
+    return base64.b64encode(realm_string)
+
+
+def main():
+    encoded_realm = encode()
+    with open('ayr-realm-b64', 'wb') as realm_string_file:
+        realm_string_file.write(encoded_realm)
+
+
+if __name__ == "__main__":
+    main()
